10703fe79fb2b8d7fe46d815bfd144e586985371
nishi0993 <nishi0993@gmail.com>
2019-03-12 14:54:28 +0530

    add getter setter for next

--- src/main/Queue.java
+++ src/main/Queue.java
@@ -18,4 +18,13 @@ public class Queue {
             this.data = data;
         }
 
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
